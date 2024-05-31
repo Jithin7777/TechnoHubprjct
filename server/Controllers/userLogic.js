e7c358c0ddec1c61ff4fc63c4c9c4ec416b7142f
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2024-05-31 16:14:46 +0530

    userLogic.js

--- server/Controllers/userLogic.js
+++ server/Controllers/userLogic.js
@@ -48,38 +48,6 @@ exports.register = async (req, res) => {
 };
 
 
-// exports.register = async (req, res) => {
-//   const { username, email, password, confirmPassword } = req.body;
-
-//   // Validate that password and confirmPassword match
-//   if (password !== confirmPassword) {
-//     return res.status(400).json("Passwords do not match.");
-//   }
-
-//   try {
-//     // Check for existing user by email first
-//     const existingUser = await users.findOne({ email });
-//     if (existingUser) {
-//       return res.status(400).json("User already exists! Please login.");
-//     }
-
-//     const salt = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash(password, salt);
-
-//     const newUser = new users({
-//       username,
-//       email,
-//       password: hashedPassword
-//     });
-
-//     await newUser.save();
-
-//     res.status(200).json(newUser);
-//   } catch (error) {
-//     res.status(500).json(`Register API failed: ${error.message}`);
-//   }
-// };
-
 
 exports.login = async (req, res) => {
     const { email, password } = req.body;
