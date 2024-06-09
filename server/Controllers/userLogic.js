9d286eb7d048598c756c52c4b1215994e7dacbed
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2024-06-09 21:29:05 +0530

    userLogic.js

--- server/Controllers/userLogic.js
+++ server/Controllers/userLogic.js
@@ -47,44 +47,6 @@ exports.register = async (req, res) => {
 };
 
 
-// exports.register = async (req, res) => {
-//   const { username, email, password, confirmPassword, role } = req.body;
-
-//   // Validate that password and confirmPassword match
-//   if (password !== confirmPassword) {
-//     return res.status(400).json("Passwords do not match.");
-//   }
-
-//   try {
-//     // Check for existing user by email first
-//     const existingUser = await users.findOne({ email });
-//     if (existingUser) {
-//       return res.status(400).json("User already exists! Please login.");
-//     }
-
-//     // Hash the password
-//     const salt = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash(password, salt);
-
-//     // Create a new user with the specified role
-//     const newUser = new users({
-//       username,
-//       email,
-//       password: hashedPassword,
-//       role, // Set the role here
-//     });
-
-//     // Save the new user to the database
-//     await newUser.save();
-
-//     // Respond with the new user's data
-//     res.status(200).json(newUser);
-//   } catch (error) {
-//     res.status(500).json(`Register API failed: ${error.message}`);
-//   }
-// };
-
-
 
 
 
@@ -126,85 +88,7 @@ exports.dummyAPI = async (req, res) => {
 }
 
 
-//email config
-// const transporter=nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 587,
-//   secure: false,
-//   service:"gmail",
-//   auth:{
-//     user:process.env.EMAIL,
-//     pass:process.env.PASSWORD,
-//   }
-// })
 
-
-
-// exports.userotpSend=async(req,res)=>{
-//   const {email}=req.body;
-//   if(!email){
-//     res.status(400).json({error:"please enter your email"})
-//   }
-//   try {
-//     const preuser=await users.findOne({email:email});
-
-//     if(preuser){
-//    const OTP=Math.floor(10000+Math.random()*900000);
-//    const existEmail=await userotp.findOne({email:email});
-//    if(existEmail){
-//     const updateData=await userotp.findByIdAndUpdate({_id:existEmail._id},{
-//       otp:OTP
-//     },{new:true}
-//   );
-//   await updateData.save();
-
-//   const mailOptions={
-//     from:process.env.EMAIL,
-//     to:email,
-//     subject:'Sending email for OTP validation',
-//     text:`OTP:-${OTP}`
-//   }
-// transporter.sendMail(mailOptions,(error,info)=>{
-//   if(error){
-//     console.log("error",error);
-//     res.status(400).json({error:"email not send"})
-//   }else{
-//     console.log("Email sent",info.response);
-//     res.status(200).json({msg:"Email sent successfully"})
-//   }
-// })
-
-//    }else{
-//     const saveOtpData=new userotp({
-//       email,otp:OTP
-//     })
-//     await saveOtpData.save();
-   
-//     const mailOptions={
-//       from:process.env.EMAIL,
-//       to:email,
-//       subject:'Sending email for OTP validation',
-//       text:`OTP:-${OTP}`
-//     }
-//   transporter.sendMail(mailOptions,(error,info)=>{
-//     if(error){
-//       console.log("error",error);
-//       res.status(400).json({error:"email not send"})
-//     }else{
-//       console.log("Email sent",info.response);
-//       res.status(200).json({msg:"Email sent successfully"})
-//     }
-//   })
-  
-//    }
-//     }
-//     else{
-//       res.status(400).json({error:"this user is not exist in your db"})
-//     }
-//   } catch (error) {
-//     res.status(400).json({error:"invalid Details",error})
-//   }
-// }
 
 exports.admin=async(req,res)=>{
   res.status(200).json("admin registred successfully")
